e9961b792ca886d0d9d79428abfde307df76a32d
Artem Galoyan <rakkii7777@gmail.com>
2024-12-23 01:25:29 +0300

    feat: added realtime directory list edit

--- watcher.py
+++ watcher.py
@@ -11,6 +11,55 @@ logging.basicConfig(
     filename="logs/general.log",
     level=logging.INFO)
 
+def directory_import():
+    try:
+        with open("directories.json", "r") as f:
+                directories = json.load(f)["directories"]
+                if not directories:
+                    logging.error(f"List are empty: ready for directories");
+                else:
+                    return directories
+    except FileNotFoundError:
+            logging.error(f"Drictories file not found");
+
+class Watcher:
+    def __init__(self):
+        self.directories = directory_import()
+        self.base_file ='directories.json'
+        
+        self.observer = Observer()
+        
+    def run(self):
+        observer = Observer()
+        observer.schedule(DirectoryHandler(), path=self.base_file, recursive=False)
+        
+        for directory in self.directories:
+            observer.schedule(Handler(), path=directory, recursive=True)
+        observer.start()
+
+        try:
+            while True:
+                time.sleep(0.1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
+        
+    def update(self, new_dict):
+        new_set_dict = set(new_dict)
+        old_set_dict = set(self.directories)
+        
+        added = new_set_dict - old_set_dict
+        removed = old_set_dict - new_set_dict
+        
+        self.observer.schedule(DirectoryHandler(), path=self.base_file, recursive=True)
+            
+        if added:
+            logging.info(f"Directory list modified - Added: {added}")
+        if removed:
+            logging.info(f"Directory list modified - Removed: {removed}")
+        
+        self.directories = new_set_dict
+
 class Handler(FileSystemEventHandler):
     def on_created(self, event):
         logging.info(f"Created - {event.src_path}")
@@ -24,18 +73,11 @@ class Handler(FileSystemEventHandler):
     def on_modified(self, event):
         logging.info(f"Modified - {event.src_path}")
         
+class DirectoryHandler(FileSystemEventHandler, Watcher):
+    def on_modified(self, event):
+        new_dict = directory_import()
+        self.update(new_dict)
+        
 if __name__ == "__main__":
-    with open("directories.json", "r") as f:
-        directories = json.load(f)["directories"]
-    
-    observer = Observer()
-    for directory in directories:
-        observer.schedule(Handler(), path=directory, recursive=True)
-    observer.start()
-    
-    try:
-        while True:
-            time.sleep(0.1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+    watcher = Watcher()
+    watcher.run()
