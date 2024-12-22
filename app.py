4f3122dd98bcdaaf9e2d8d487e429c938a9ca8b9
Artem Galoyan <rakkii7777@gmail.com>
2024-12-23 02:06:44 +0300

    feat: route output the list of all files and directories

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 import os
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from fastapi.responses import JSONResponse
 import uvicorn
+
+from watcher import directory_import
 
 app = FastAPI()
 
 @app.get("/")
 def file_watcher_status():
-    text = "Hello, World!"
-    return {"text": text}
+    contents = {}
+    
+    directory_list = directory_import()
+    print(directory_list)
+    for directory in directory_list:
+        print(directory)
+        contents.update({directory: os.listdir(directory)})
+        
+    return JSONResponse(content=contents)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv('APP_PORT')))
+    uvicorn.run(app, host="0.0.0.0", port=8127)
